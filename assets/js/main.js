870195197ae1e59de0430df12153cbca765c3a4e
tkknopka <tani20@yandex.ru>
2024-01-22 17:40:47 +0700

    первый коммит
    для проекта создан репозиторий гит

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// кастомные функции
+jQuery(function ($) {
+    // mobile menu
+    $(document).ready(function () {
+        $('.header__burger').click(function (event) {
+            $('.header__burger, .mobile-menu').toggleClass('active');
+            $('.container').toggleClass('lock');
+        });
+        $('.header__list').click(function (event) {
+            $('.header__burger, .mobile-menu').removeClass('active');
+            $('.container').removeClass('lock');
+        });
+    });
+});
