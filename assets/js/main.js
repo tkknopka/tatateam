a9d7f5af8f84f76bc9e70f1071bb572689b6a2a9
tkknopka <tani20@yandex.ru>
2024-02-22 00:54:04 +0700

    отредактированы бегущие строки
    добавлен html-тег marquee

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,35 +13,35 @@ jQuery(function ($) {
     });
 });
 // Swiper
-document.addEventListener('DOMContentLoaded', function () {
-    /** clientele-slider */
-    let swiper2 = new Swiper('.clientele-slider', {
-        direction: 'horizontal',
-        slidesPerView: 5,
-        // Optional parameters
-        loop: true,
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false
-        },
-        speed: 1500,
-        breakpoints: { // условия для разных размеров окна браузера
-            320: {
-                slidesPerView: 1,
-            },
-            479.98: { // при 479.98px и выше
-                slidesPerView: 2,
-            },
-            768: { // при 768px и выше
-                slidesPerView: 2,
-            },
-            1000: { // при 768px и выше
-                slidesPerView: 3,
-            },
-            1280: { // при 768px и выше
-                slidesPerView: 5,
-            }
-        },
+// document.addEventListener('DOMContentLoaded', function () {
+//     /** clientele-slider */
+//     let swiper2 = new Swiper('.clientele-slider', {
+//         direction: 'horizontal',
+//         slidesPerView: 5,
+//         // Optional parameters
+//         loop: true,
+//         autoplay: {
+//             delay: 3000,
+//             disableOnInteraction: false
+//         },
+//         speed: 1500,
+//         breakpoints: { // условия для разных размеров окна браузера
+//             320: {
+//                 slidesPerView: 1,
+//             },
+//             479.98: { // при 479.98px и выше
+//                 slidesPerView: 2,
+//             },
+//             768: { // при 768px и выше
+//                 slidesPerView: 2,
+//             },
+//             1000: { // при 768px и выше
+//                 slidesPerView: 3,
+//             },
+//             1280: { // при 768px и выше
+//                 slidesPerView: 5,
+//             }
+//         },
 
-    });
-})
+//     });
+// })
